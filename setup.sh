86fdfef5de4ac47c8d9548259d7a262901c6fcea
Stefan Rares <srares@192-168-0-226.rdsnet.ro>
2021-07-03 01:14:24 +0300

    added automatic VSCode extension installation + updated README

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
-# Install brew
-brew help &> /dev/null || ./install-brew.sh
-
+# Install brew if not already installed
+brew help &> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install wget
 brew install wget
 
-
 # Download all the scripts
-
 if [ ! -d ~/scripts ]
 then
 	mkdir ~/scripts
@@ -23,9 +20,7 @@ wget https://raw.githubusercontent.com/srares76/scripts-repo/master/brght
 wget https://raw.githubusercontent.com/srares76/scripts-repo/master/upgrade-utilities.sh
 sudo chmod a+x * 
 
-
 # Move them in bin
-
 for each in $(ls ~/scripts)
 do
 	if [ ! -f /usr/local/bin/"$each" ]
@@ -37,7 +32,6 @@ done
 clear
 
 # Possibly install Sublime
-
 echo "Do you want Sublime-Text?"
 read ANS
 if [ "$ANS" = "yes" ] || [ "$ANS" = "Yes" ] || [ "$ANS" = "y" ] || [ "$ANS" = "Y" ]
@@ -46,16 +40,25 @@ then
 fi
 
 # Possibly install Visual Studio Code
-
 echo "Do you want Visual Studio Code?"
 read ANS
 if [ "$ANS" = "yes" ] || [ "$ANS" = "Yes" ] || [ "$ANS" = "y" ] || [ "$ANS" = "Y" ]
 then
 	brew install visual-studio-code
+
+	# Installation of my most used VSCode extensions
+	code --install-extension apommel.matlab-interactive-terminal
+	code --install-extension bramvanbilsen.matlab-code-run
+	code --install-extension CoenraadS.bracket-pair-colorizer-2
+	code --install-extension Gimly81.matlab
+	code --install-extension kiteco.kite
+	code --install-extension ms-vscode.cpptools
+	code --install-extension paulosilva.vsc-octave-debugger
+	code --install-extension thanhtrang.theme-flatland-monokai
+	code --install-extension toasty-technologies.octave
 fi
 
 # Possibly install Discord
-
 echo "Do you want Discord?"
 read ANS
 if [ "$ANS" = "yes" ] || [ "$ANS" = "Yes" ] || [ "$ANS" = "y" ] || [ "$ANS" = "Y" ]
@@ -63,6 +66,7 @@ then
 	brew install Discord
 fi
 
+# Possibly install Google Chrome
 echo "Do you want Google Chrome?"
 read ANS
 if [ "$ANS" = "yes" ] || [ "$ANS" = "Yes" ] || [ "$ANS" = "y" ] || [ "$ANS" = "Y" ]
@@ -70,9 +74,15 @@ then
 	brew install google-chrome
 fi
 
+# Possibly intall Octave
+echo "Do you want Octave for terminal?"
+read ANS
+if [ "$ANS" = "yes" ] || [ "$ANS" = "Yes" ] || [ "$ANS" = "y" ] || [ "$ANS" = "Y" ]
+then
+	brew install octave
+fi
 
 # Install essentials
-
 echo "Installing essentials..."
 sleep 2
 brew install whatsapp
@@ -81,9 +91,7 @@ brew install alfred
 brew install spectacle
 brew install macs-fan-control
 
-
 # Install other packages
-
 clear
 echo "Do you ever use the terminal?"
 read ANS
@@ -101,9 +109,7 @@ then
 	brew install brightness
 fi
 
-
 # Clean up
-
 rm -r ~/scripts
 
 echo =========================================================
